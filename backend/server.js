e515bda78139ce96bb7243c91041b1e5ab93e1d7
Sai Seelam <saiseelam446@gmail.com>
2025-05-03 20:41:24 -0500

    Server Online Fix

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ app.post('/login', async (req, res) => {
 });
 
 // Route to get user details
-app.get('/user', (req, res) => {
+app.get('/users', (req, res) => {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized' });

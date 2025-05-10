367ecfba2342386eae1236e92655783ac2ff6198
Sai Seelam <saiseelam446@gmail.com>
2025-05-09 22:48:11 -0500

    Vercel Attempt #2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 
 // ✅ Fix CORS issue: Allow `localhost` and `127.0.0.1`
 app.use(cors({
-  origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+  origin: ["http://127.0.0.1:5500", "http://localhost:5500", "https://volunteerhub-bay.vercel.app"],
   credentials: true,
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"]

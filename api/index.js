83e139db494377c8a17a60acdf1312d8d19016b4
Sai Seelam <saiseelam446@gmail.com>
2025-05-26 13:52:43 -0500

    i dont understand vercel

--- api/index.js
+++
@@ -1,4 +0,0 @@
-// Vercel serverless function for root API endpoint
-export default function handler(req, res) {
-  res.status(200).json({ message: 'VolunteerHub API root. See documentation for available endpoints.' });
-}
